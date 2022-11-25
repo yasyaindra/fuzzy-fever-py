75de0c143df0d1119d1bb8be6cf80e583e88a013
yasyaindra <indrayasya96@gmail.com>
2022-11-25 16:09:18 +0800

    add visualization in readme

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Fuzzy Fever System</h1>
+
+<img src="/figure.png">
--- visualize.py
+++ visualize.py
@@ -3,6 +3,7 @@ plt.style.use('seaborn-whitegrid')
 import numpy as np
 
 fig = plt.figure()
+plt.ylim(0,5)
 x = np.linspace(0, 10, 100)
 plt.plot(x+36, x + 0, linestyle='solid')
 plt.axhline(y=1, color='r', linestyle=':')
